51aa436c5fa57b3901ad54367b47ac7d47819041
tong12580 <yutong@rrjc.com>
2018-03-09 15:57:17 +0800

    新增war包发布法

---
+++ src/main/java/com/business/ServletInitializer.java
@@ -0,0 +1,15 @@
+//package com.business;
+//
+//import org.springframework.boot.builder.SpringApplicationBuilder;
+//import org.springframework.boot.web.support.SpringBootServletInitializer;
+//
+///**
+// * ServletInitializer
+// * Created by yuTong on 2018/3/9.
+// */
+//public class ServletInitializer extends SpringBootServletInitializer {
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//        return application.sources(OutsourcedProjectApplication.class);
+//    }
+//}
