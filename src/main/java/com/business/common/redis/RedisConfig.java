eb1e15d05ee5360faa477a362cc85568f92d5aff
余童 <467942250@qq.com>
2019-12-05 16:08:27 +0800

    初始化数据

--- src/main/java/com/business/common/redis/RedisConfig.java
+++ src/main/java/com/business/common/redis/RedisConfig.java
@@ -1,21 +1,30 @@
 package com.business.common.redis;
 
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.cache.RedisCacheWriter;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import javax.annotation.Resource;
+import java.time.Duration;
 
 /**
  * @author yuton
  * @version 1.0
  * @since 2017/1/16 9:34
  */
+@EnableCaching
 @Configuration
-public class RedisConfig {
+public class RedisConfig extends CachingConfigurerSupport {
     @Resource
     private RedisConnectionFactory redisConnectionFactory;
 
@@ -30,4 +39,22 @@ public class RedisConfig {
         template.afterPropertiesSet();
         return template;
     }
+
+    @Bean
+    public CacheManager cacheManager() {
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration
+                .defaultCacheConfig()
+                .serializeKeysWith(RedisSerializationContext
+                        .SerializationPair
+                        .fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext
+                        .SerializationPair
+                        .fromSerializer(new GenericJackson2JsonRedisSerializer()))
+                .entryTtl(Duration.ofHours(1));
+        return RedisCacheManager
+                .builder(RedisCacheWriter.nonLockingRedisCacheWriter(redisConnectionFactory))
+                .cacheDefaults(redisCacheConfiguration)
+                .build();
+    }
+
 }
--- src/test/java/com/business/OutsourcedProjectApplicationTests.java
+++ src/test/java/com/business/OutsourcedProjectApplicationTests.java
@@ -1,15 +1,26 @@
 package com.business;
 
+import com.business.common.redis.RedisUtil;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.annotation.Resource;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class OutsourcedProjectApplicationTests {
+
+    @Resource
+    private RedisUtil<String, String> redisUtil;
+
     @Test
     public void contextLoads() {
     }
 
+    @Test
+    public void testRedis() {
+        System.out.println(redisUtil.ping());
+    }
 }
