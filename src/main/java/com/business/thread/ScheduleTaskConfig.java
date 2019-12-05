7feb9e45a7480941cdbec6180902b82f89a5aaec
余童 <467942250@qq.com>
2019-12-05 10:50:47 +0800

    优化代码

--- src/main/java/com/business/thread/ScheduleTaskConfig.java
+++ src/main/java/com/business/thread/ScheduleTaskConfig.java
@@ -1,5 +1,6 @@
 package com.business.thread;
 
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +10,10 @@ import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.ThreadPoolExecutor;
+
+import static java.util.concurrent.TimeUnit.NANOSECONDS;
 
 /**
  * Created by yuton on 2016/9/15.
@@ -25,6 +30,8 @@ public class ScheduleTaskConfig implements SchedulingConfigurer {
 
     @Bean
     public ExecutorService taskExecutor() {
-        return Executors.newScheduledThreadPool(100);
+        return new ScheduledThreadPoolExecutor(2,  new ThreadFactoryBuilder()
+                .setNameFormat("scheduleTaskExecutor-fixed-%d")
+                .build());
     }
 }
--- src/main/java/com/business/thread/TaskExecutorConfig.java
+++ src/main/java/com/business/thread/TaskExecutorConfig.java
@@ -1,13 +1,16 @@
 package com.business.thread;
 
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.Executor;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by yuton on 2016/9/15.
@@ -15,16 +18,15 @@ import java.util.concurrent.Executor;
 @Configuration
 @EnableAsync
 @ComponentScan
-public class TaskExecutorConfig implements AsyncConfigurer{
+public class TaskExecutorConfig implements AsyncConfigurer {
+
     @Override
     public Executor getAsyncExecutor() {
-        ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
-        taskExecutor.setCorePoolSize(5);
-        taskExecutor.setMaxPoolSize(500);
-        taskExecutor.setQueueCapacity(600);
-        taskExecutor.setKeepAliveSeconds(10);
-        taskExecutor.initialize();
-        return taskExecutor;
+        return new ThreadPoolExecutor(2, 4, 0, TimeUnit.MILLISECONDS,
+                new LinkedBlockingQueue<>(100),
+                new ThreadFactoryBuilder()
+                        .setNameFormat("taskExecutor-fixed-%d")
+                        .build());
     }
 
     @Override
